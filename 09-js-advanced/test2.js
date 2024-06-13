a08ca3c8714c60e9a5180488899ab8ec95e3fadd
aixion1506 <aixion1506@gmail.com>
2024-06-13 15:32:37 +0900

    직사각형 클래스 만들기

--- 09-js-advanced/test2.js
+++ 09-js-advanced/test2.js
@@ -11,3 +11,18 @@ class Shape {
 let rec1 = new Shape(3,4);
 console.log(rec1.getArea());
 
+
+class Rectangle extends Shape {
+    constructor (width, heigth)  {
+        super(width, heigth);
+    }
+    getDiagonal() {
+        return Math.sqrt(this.width**2 + this.height**2)
+        // Math.sqrt() : 제곱근을 구하는 함수
+        
+    }
+
+}
+
+let rec2 = new Rectangle(4, 5);
+console.log(rec2.getDiagonal());
