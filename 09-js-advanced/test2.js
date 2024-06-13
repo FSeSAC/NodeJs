c6f06d60950f14a39efa7444a7409109d3666a30
aixion1506 <aixion1506@gmail.com>
2024-06-13 15:46:41 +0900

    원 클래스 만들기

--- 09-js-advanced/test2.js
+++ 09-js-advanced/test2.js
@@ -10,8 +10,9 @@ class Shape {
 
 let rec1 = new Shape(3,4);
 console.log(rec1.getArea());
+console.log('---------------');
 
-
+// 직사각형 클래스
 class Rectangle extends Shape {
     constructor (width, heigth)  {
         super(width, heigth);
@@ -26,3 +27,35 @@ class Rectangle extends Shape {
 
 let rec2 = new Rectangle(4, 5);
 console.log(rec2.getDiagonal());
+console.log(rec2.getArea());
+
+console.log('---------------');
+
+// 삼각형 클래스
+class Triangle extends Shape {
+    constructor (width, height) {
+        super(width, height);
+    }
+    // 오버라이딩: 부모 클래스랑 동일한 메서드 존재시 본인의 메서드 우선 사용
+    getArea() {
+        return this.width * this.height / 2
+    }
+}
+
+let tri = new Triangle(4,5);
+console.log(tri.getArea());
+console.log('---------------');
+
+// 원 클래스
+class Circle extends Shape {
+    constructor (width, height, radius) {
+        super(width, height);
+        this.radius = radius;
+    }
+    getArea() {
+        return this.radius**2 * Math.PI
+    }
+}
+
+let cir = new Circle(2,2,3);
+console.log(cir.getArea());
