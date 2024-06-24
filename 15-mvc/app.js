28ed4ea017ae58247f4b69d4d31df25fc35e284f
aixion1506 <aixion1506@gmail.com>
2024-06-24 15:16:00 +0900

    MVC 패턴 분리 완료

---
+++ 15-mvc/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views'); 
+
+const idxRouter = require('./routes/index');
+app.use('/', idxRouter);
+
+
+// 404
+// 맨 마지막에 라우트/주소로 선언: 맨 마지막이 아니면 404이후의 다른 라우팅들이 무시
+app.get('*', (req, res) => {
+    res.render('404');
+})
+
+app.listen(PORT, ()=> {
+    console.log(`http://localhost:${PORT}`);
+});
