66ef98e3cd67d06bbe6caba272ce1cc87f14c9f3
aixion1506 <aixion1506@gmail.com>
2024-06-24 20:57:19 +0900

    mysql 연결 전

---
+++ 16-mvc-mysql/model/Visitor.js
@@ -0,0 +1,30 @@
+// // (임시) DB로부터 방명록 데이터를 받아옴
+// exports.getVisitors = () => {
+//     return [
+//       { id: 1, name: '홍길동', comment: '내가 왔다.' },
+//       { id: 2, name: '이찬혁', comment: '으라차차' },
+//     ];
+// };    
+
+
+const mysql = require('mysql');
+
+// DB 연결 객체 - conn
+const conn = mysql.createConnection({
+  host:'localhost',
+  user: 'user',
+  pw: '1234',
+  database: 'codingon'
+});
+
+
+exports.getVisitors = (callback) => {
+  conn.query(`SELECT * FROM visitor`, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('model/visitors.js ->', rows);
+    callback(rows);
+  })
+}
