831acf9d9a4ed2165f91da0b0da71b3634be0d7a
aixion <aixion1506@gmail.com>
2024-06-29 15:36:21 +0900

    model getUser 추가

--- 16-MySQL-Login/model/User.js
+++ 16-MySQL-Login/model/User.js
@@ -9,10 +9,26 @@ const conn = mysql.createConnection({
 
 // 회원 가입
 exports.postUser = (data, callback) => {
-    conn.query(`insert into user(userid, name, pw) values('${data.userid}', '${data.name}', '${data.password} )`, (err, rows) => {
+    conn.query(`insert into user(userid, name, password) values('${data.userid}', '${data.name}', '${data.password} )`, (err, rows) => {
         if (err) throw err;
         
-        console.log('Model/postUser ->', rows);
+        console.log('model/postUser ->', rows);
         callback(rows);
     } )
+}
+
+
+
+// 로그인 회원 조회
+exports.getUser = (data, callback) => {
+    let query;
+    if('password' in data) query = `select * from user where userid = '${data.userid}' and password = '${data.password}'`
+    else query = `select * from user where userid='${data.userid}'`
+
+    conn.query(`select * from user`, (err, rows) => {
+        if (err) throw err;
+
+        console.log('model/getUser ->', rows);
+        callback(rows[0]);
+    })
 }
