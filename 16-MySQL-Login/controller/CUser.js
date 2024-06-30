398c0607565eed54182db90ef37650b9e1caceb8
aixion <aixion1506@gmail.com>
2024-06-30 23:41:04 +0900

    controller 추가

--- 16-MySQL-Login/controller/CUser.js
+++ 16-MySQL-Login/controller/CUser.js
@@ -1,6 +1,27 @@
 const User = require('../model/User');
 
 
+// 메인 페이지
 exports.main = (req, res) => {
     res.render('index');
   };
+
+
+// 회원가입 페이지
+exports.signupPage = (req, res) => {
+  res.render('signup');
+}
+
+// 회원 가입하기
+exports.postUser = (req, res) => {
+  console.log('req.body', req.body);
+  User.postUser(req.body, (result) => {
+    console.log('controller/postUser ->', result);
+    res.send({ id: result });
+  })
+}
+
+
+exports.signinPage = (req, res) => {
+  res.render('signin');
+}
--- 16-MySQL-Login/routes/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const controller = require('../controller/CUser');
-const router = express.Router();
-
-
-router.get('/', controller.main);
