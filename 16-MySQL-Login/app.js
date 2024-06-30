4eff5bdeb1069dbe185a8f036202bf12fff8551f
aixion <aixion1506@gmail.com>
2024-06-30 23:40:24 +0900

    기본 url `/user`추가

--- 16-MySQL-Login/app.js
+++ 16-MySQL-Login/app.js
@@ -2,22 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
-
 app.set('view engine', 'ejs');
 app.set('views', './views'); 
 app.use('/static', express.static(__dirname + '/static'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const indexRouter = require('./routes'); // index는 생략 가능!
-app.use('/', indexRouter); // localhost:PORT/ 경로를 기본으로 ./routes/index.js 파일에 선언한 대로 동작
+const userRouter = require('./routes/user');
+app.use('/user', userRouter); // localhost:PORT/ 경로를 기본으로 ./routes/index.js 파일에 선언한 대로 동작
 
 app.get('*', (req, res) => {
     res.render('404');
   });
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}/user`);
   });
   
   
